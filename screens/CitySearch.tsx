60b21c7a5d23bef5bbb5fa8e0d421c223d6c1a95
Bob Lam <boblam.ceo@gmail.com>
2022-06-11 14:42:50 +0800

    first commit

---
+++ screens/CitySearch.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { Text, View } from "react-native";
+
+export const CitySearch: React.SFC<{}> = () => {
+  return (
+    <View>
+      <Text>HELLO WORLD CITY SEARCH BSLFOA</Text>
+    </View>
+  );
+};
